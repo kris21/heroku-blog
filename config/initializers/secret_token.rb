0ec84f0171f0441e108db1643b68683cd2fc6f13
Kris He <kris.he@zuora.com>
2011-09-21 15:00:13 +0800

    blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '29f284c4645e00d56655c128fdb70c80cb5400454c875c65d5704e70380e4f9ad482ae96de4559546bb75dcf6a925cabb84588a562edf532c5dcfd4d26ac8b48'
